ffe5e4be02bae77dd1faa8d63ffd982dac660d37
John Papa <john@badfishapps.com>
2016-12-24 14:43:30 -0700

    Initial commit

---
+++ cards/card.py
@@ -0,0 +1,31 @@
+"""
+Created on Dec 04, 2016
+
+@author: john papa
+
+Copyright 2016 John Papa.  All rights reserved.
+This work is licensed under the MIT License.
+"""
+from abc import ABCMeta, abstractproperty
+
+class Card(metaclass=ABCMeta):
+
+    def __init__(self, suit, rank):
+        self._rank = rank
+        self._suit = suit
+        self._value = None
+
+    def __str__(self):
+        return f"{self._rank} of {self._suit}"
+
+    @property
+    def rank(self):
+        return self._rank
+
+    @property
+    def suit(self):
+        return self._suit
+
+    @abstractproperty
+    def value(self):
+        """ Returns the value of the card used for scoring the game """
---
+++ tests/test_card.py
@@ -0,0 +1,51 @@
+"""
+Created on Dec 04, 2016
+
+@author: john papa
+
+Copyright 2016 John Papa.  All rights reserved.
+This work is licensed under the MIT License.
+"""
+import unittest
+
+from cards.card import Card
+from unittest.mock import patch, PropertyMock, MagicMock
+
+
+class Test_Card(unittest.TestCase):
+    def setUp(self):
+        self._suit = "clubs"
+        self._rank = "10"
+        self._card = ConcreteCard(suit=self._suit, rank=self._rank)
+
+    def tearDown(self):
+        pass
+
+    def test_card_is_abstract_class(self):
+        """ Test that the Card class is an abstract base class """
+        with self.assertRaises(TypeError):
+            Card()
+
+    def test_rank(self):
+        """ Test 'rank' property returns correct rank. """
+        card = self._card
+        self.assertEqual(card.rank, self._rank)
+
+    def test_suit(self):
+        """ Test 'suit' property returns correct suit. """
+        card = self._card
+        self.assertEqual(card.suit, self._suit)
+
+
+class ConcreteCard(Card):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @property
+    def value(self):
+        pass
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
