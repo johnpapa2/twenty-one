ab75f4256afa3605255afdb3c47508ddbdde9a4c
John Papa <john@badfishapps.com>
2016-12-29 13:37:42 -0700

    Changed position to role. Return string from display_hand

--- players/player.py
+++ players/player.py
@@ -14,18 +14,18 @@ from cards.hand import Hand
 
 class Player():
 
-    def __init__(self, name, position='Player'):
+    def __init__(self, name, role='Player'):
         self._name = name
-        self._position = position
+        self._role = role
         self._hand = Hand()
         self._bankroll = 1000
         self._busted = False
         self._wins = 0
         self._losses = 0
         self._logger = logging.getLogger('bj')
-        if position == 'Dealer':
+        if role == 'Dealer':
             self._logger.info(f"{self} taps into table")
-        elif position == 'Player':
+        elif role == 'Player':
             self._logger.info(f"{self} sits at table")
 
     @property
@@ -37,7 +37,7 @@ class Player():
         self._bankroll = value
 
     def __str__(self):
-        return f"{self.position} {self.name}"
+        return f"{self.role} {self.name}"
 
     @property
     def bet(self):
@@ -49,6 +49,8 @@ class Player():
 
     @busted.setter
     def busted(self, value):
+        if not isinstance(value, bool):
+            raise ValueError()
         self._busted = value
 
     @property
@@ -68,8 +70,8 @@ class Player():
         return self._name
 
     @property
-    def position(self):
-        return self._position
+    def role(self):
+        return self._role
 
     @property
     def total(self):
@@ -86,9 +88,8 @@ class Player():
         self._wins = value
 
     def display_hand(self):
-        ranks = [card.rank for card in self.hand]
-        hand = '] ['.join(ranks)
-        self._logger.info(f"{self} has [{hand}]")
+        hand_display = f"{self} has {self.hand}"
+        return hand_display
 
     def empty_hand(self):
         self._hand = Hand()
@@ -96,22 +97,22 @@ class Player():
     def move(self, deck, dealers_hand):
         move = None
 
-        if self.position == 'Player':
+        if self.role == 'Player':
             self._logger.info(f"Dealer shows a [{dealers_hand[0].rank}]")
-            self.display_hand()
+            self._logger.info(self.display_hand())
             if self.total != 21:
                 while move != 'stand':
                     move = click.prompt(f"Hand total {self.total}, Your Move", default='stand')
                     if move == 'hit':
                         self._logger.info(f"{self} hits")
                         self.receives(deck.deal_card())
-                        self.display_hand()
+                        self._logger.info(self.display_hand())
                     elif move == 'double':
                         self._logger.info(f"{self} doubles")
                         self.hand.bet += self.hand.bet
                         self.bankroll -= self.hand.bet
                         self.receives(deck.deal_card())
-                        self.display_hand()
+                        self._logger.info(self.display_hand())
                         break
                     if self.total > 21:
                         self.busted = True
@@ -120,10 +121,10 @@ class Player():
                 if move == 'stand':
                     self._logger.info(f"{self} stands!")
         else:
-            self.display_hand()
+            self._logger.info(self.display_hand())
             while self.total < 17:
                 self.receives(deck.deal_card())
-                self.display_hand()
+                self._logger.info(self.display_hand())
                 if self.total > 21:
                     self.busted = True
                     break
@@ -141,5 +142,5 @@ class Player():
 
     def receives(self, card):
         self.hand.add_card(card)
-        if self.position != 'Discard':
+        if self.role != 'Discard':
             self._logger.debug(f"{self} gets {card}")
